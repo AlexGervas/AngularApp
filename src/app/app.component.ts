27844dba3345df362782205d1e854a2f4bdfa8ef
AleksKirienko <aleks.Kirienko@bk.ru>
2020-06-19 23:22:29 +0700

    Update files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: `<label>Введите имя:</label>
-    <input [(ngModel)]="name" placeholder="name">
-    <h1>Добро пожаловать {{name}}!</h1>`
+  selector: 'my-app',
+  template: `<label>Введите имя:</label>
+  <input [(ngModel)]="name" placeholder="name">
+  <h1>Добро пожаловать {{name}}!</h1>`
 })
 export class AppComponent {
-    name = '';
+  name = '';
+  title = 'AngularApp';
 }
