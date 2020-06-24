32be6bf169e821e9a2bde1b1d9dede4ddb4eb7f5
AleksKirienko <aleks.Kirienko@bk.ru>
2020-06-24 19:19:00 +0700

    Use ready-made style component library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, NgbModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
