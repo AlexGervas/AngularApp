aa09eea9ef20c8e63d63a529d2459d9605ca9619
AleksKirienko <aleks.Kirienko@bk.ru>
2020-07-07 21:59:59 +0700

    Add handler error 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,19 +12,21 @@ import { CompanyComponent } from './components/company/company.component';
 import { ShopListComponent } from './components/shop-list/shop-list.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { NewsComponent } from './components/news/news.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 // определение маршрутов
 const appRoutes: Routes = [
   {path: '', component: MainPageComponent},
   {path: 'home', component: MainPageComponent},
   {path: 'news', component: NewsComponent},
-  {path: 'shop-list', component: ShopListComponent}
+  {path: 'shop-list', component: ShopListComponent},
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
   // Импортируем CoreModule вместе со всеми сервисами что там есть
   imports: [BrowserModule, FormsModule, NgbModule, HttpClientModule, CoreModule, RouterModule.forRoot(appRoutes)],
-  declarations: [AppComponent, MenuComponent, CompanyComponent, ShopListComponent, MainPageComponent, NewsComponent],
+  declarations: [AppComponent, MenuComponent, CompanyComponent, ShopListComponent, MainPageComponent, NewsComponent, NotFoundComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
