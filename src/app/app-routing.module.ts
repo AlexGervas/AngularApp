41aa02c76871fcda0433a95e3ea03eace6fbfbe9
AleksKirienko <aleks.Kirienko@bk.ru>
2020-07-08 15:52:01 +0700

    Transfer from app.modul to app-routing.module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CoreModule } from './core/core.module';
+
+import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { CompanyComponent } from './components/company/company.component';
+import { ShopListComponent } from './components/shop-list/shop-list.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { NewsComponent } from './components/news/news.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+// определение маршрутов
+const appRoutes: Routes = [
+  {path: '', component: MainPageComponent},
+  {path: 'home', component: MainPageComponent},
+  {path: 'news', component: NewsComponent},
+  {path: 'shop-list', component: ShopListComponent},
+  {path: '**', component: NotFoundComponent}
+];
+
+@NgModule({
+  // Импортируем CoreModule вместе со всеми сервисами что там есть
+  imports: [BrowserModule, FormsModule, NgbModule, HttpClientModule, CoreModule, RouterModule.forRoot(appRoutes)],
+  declarations: [AppComponent, MenuComponent, CompanyComponent, ShopListComponent, MainPageComponent, NewsComponent, NotFoundComponent],
+})
+
+export class AppRoutingModule {
+}
